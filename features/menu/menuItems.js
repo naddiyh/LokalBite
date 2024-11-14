da6c387ed7e87c9e876be14b7bc4b0c0bbe6006d
AyyasTakim <takimayyas@gmail.com>
2024-11-14 17:37:28 +0800

    update menu page

---
+++ features/menu/menuItems.js
@@ -0,0 +1,72 @@
+export const menuItems = [
+  {
+    id: 1,
+    title: "Coto Makassar",
+    description:
+      "Hidangan khas Makassar berupa sup daging sapi dengan bumbu kacang yang kaya rempah.",
+    price: "Rp30.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 2,
+    title: "Konro",
+    description:
+      "Sup iga sapi khas Makassar dengan bumbu kluwak yang kaya rasa.",
+    price: "Rp40.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 3,
+    title: "Pallubasa",
+    description:
+      "Sup daging sapi berkuah kental dengan kelapa parut dan bumbu khas.",
+    price: "Rp35.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 4,
+    title: "Sop Saudara",
+    description:
+      "Sup berkuah gurih dengan daging sapi, bihun, dan kentang, khas Sulawesi.",
+    price: "Rp32.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 5,
+    title: "Nasu Palekko",
+    description:
+      "Hidangan ayam atau itik berbumbu khas Bugis yang pedas dan gurih.",
+    price: "Rp28.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 6,
+    title: "Kapurung",
+    description:
+      "Makanan khas Sulawesi berupa bubur sagu dengan sayuran dan ikan.",
+    price: "Rp25.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 7,
+    title: "Ayam Rica-Rica",
+    description: "Ayam pedas khas Manado yang dimasak dengan bumbu rica-rica.",
+    price: "Rp30.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 8,
+    title: "Bubur Manado",
+    description: "Bubur khas Manado yang diisi dengan sayuran segar dan ikan.",
+    price: "Rp20.000",
+    image: "/images/salad.png",
+  },
+  {
+    id: 9,
+    title: "Ikan Woku Belanga",
+    description:
+      "Ikan yang dimasak dengan bumbu woku khas Minahasa, pedas dan wangi.",
+    price: "Rp35.000",
+    image: "/images/salad.png",
+  },
+];
